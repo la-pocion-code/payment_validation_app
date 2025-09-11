9944ecbea5edbad788bac7d692a73d0c4b38f8c7
William Castro <willcr32@gmail.com>
2025-09-11 18:10:34 -0500

    prueba base10

--- financial_tracker/settings.py
+++ financial_tracker/settings.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(_file_).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
