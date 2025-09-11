d0350f671b7f69dbf77c24d4df4a961f49c085c9
William Castro <willcr32@gmail.com>
2025-09-11 16:41:00 -0500

    prueba base2

--- financial_tracker/settings.py
+++ financial_tracker/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url
 
-print(f"DEBUG: DATABASE_URL from os.getenv: {os.getenv('DATABASE_URL')}")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -88,6 +88,10 @@ WSGI_APPLICATION = 'financial_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+print("Entorno Railway:")
+for k, v in os.environ.items():
+    print(f"{k} = {v}")
 
 DATABASES = {
     'default': dj_database_url.config(
